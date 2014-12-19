c95e9c29792c4b042444d75fe691f0e307dce474
Beenish Khurshid <kbeenish@hotmail.com>
2014-12-18 18:41:29 -0700

    Use query paramaters instead of path

--- fiplus-arango-extensions/apps/databases/fiplus/extensions/match.js
+++ fiplus-arango-extensions/apps/databases/fiplus/extensions/match.js
@@ -21,26 +21,27 @@ var UserModel = foxx.Model.extend({
 	}
     });
 
-    controller.get('/event/:user_id/:num_events/:by_interest/:priority_offset', function (req, res) {
+    controller.get('/events', function (req, res) {
 
-    }).pathParam('user_id', {
+    }).queryParam('user_id', {
       type: joi.string(),
       required: true,
       description: 'The user for which we are retrieving events'
-    }).pathParam('num_events', {
+    }).queryParam('num_events', {
       type: joi.number().integer(),
       required: true,
       description: 'The number of events that are requested'
-    }).pathParam('by_interest', {
+    }).queryParam('by_interest', {
       type: joi.boolean(),
       required: false,
       description: 'If events should be filtered by user interest (false by default)'
-    }).pathParam('priority_offset', {
+    }).queryParam('priority_offset', {
       type: joi.number().integer(),
       required: false,
       description: 'The priority level to start at (zero by default). To be used when updating event list with new events and the first priority_offest number of events should be skipped.'
     }).bodyParam('location', {
       type: locationModel,
+	  required: false,
       description: 'Location near which to search for events'
     });
 }());
