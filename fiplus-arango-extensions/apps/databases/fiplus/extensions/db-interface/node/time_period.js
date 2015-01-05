d45c26fba13f40a029fdc5f2b7af69cede060125
Ralph <kim2oden@gmail.com>
2015-01-05 13:35:35 -0700

    Prevent Time period duplicates with same start and end timestamps

--- fiplus-arango-extensions/apps/databases/fiplus/extensions/db-interface/node/time_period.js
+++ fiplus-arango-extensions/apps/databases/fiplus/extensions/db-interface/node/time_period.js
@@ -14,16 +14,42 @@ var TimePeriod = function()
 };
 
 /**
- * Add a time period with start and end times
+ * Add a time period with start and end times.
  */
 TimePeriod.prototype.saveTimePeriod = function(start_time, end_time)
 {
     var result;
 
-    // Every created time period is unique
-    result = this.db.time_period.save({});
-    if(result.error == true) {
-        throw new error.GenericError('Saving time period failed.');
+    //Prevent the creation of a time period with the same start and end time as another time period.
+    var time_period_collection = this.db.time_period.toArray();
+    var time_period_collection_length = this.db.time_period.count();
+    var time_period;
+    var time_period_found;
+    for (var i=0; i < time_period_collection_length; i++)
+    {
+        time_period = time_period_collection[i];
+        this.db.start.outEdges(time_period._id).forEach(function (edge) {
+            var start_timestamp = edge._to;
+            if (start_timestamp == start_time) {
+                this.db.end.outEdges(time_period._id).forEach(function (edge) {
+                    var end_timestamp = edge._to;
+                    if (end_timestamp == end_time) {
+                        //We found an existing time_period node with the same start and end time
+                        result = time_period;
+                        time_period_found = true;
+                        break;
+                    }
+                });
+            }
+        });
+    }
+    if(time_period_found != true)
+    {
+        // Every created time period is unique
+        result = this.db.time_period.save({});
+        if (result.error == true) {
+            throw new error.GenericError('Saving time period failed.');
+        }
     }
 
     var start_edge = (new start.Start()).saveStartEdge(result._id, start_time);
