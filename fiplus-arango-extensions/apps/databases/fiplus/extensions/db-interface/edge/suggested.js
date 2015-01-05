31028dfd4a3606383e460e9832368f30d186288c
Beenish Khurshid <kbeenish@hotmail.com>
2015-01-05 11:12:56 -0700

    Add error checking to prevent duplicate suggestion

--- fiplus-arango-extensions/apps/databases/fiplus/extensions/db-interface/edge/suggested.js
+++ fiplus-arango-extensions/apps/databases/fiplus/extensions/db-interface/edge/suggested.js
@@ -24,6 +24,23 @@ Suggested.prototype.saveSuggestedTimeEdge = function(activity_id, start_time, en
     var toField = this.TO_FIELD;
     var result;
 
+    // check to see if suggestion already exists
+    db.suggested.outEdges(activity_id).forEach(function(edge) {
+        var suggestion_id = edge._to;
+        var timePeriod_id = db.is.outEdges(suggestion_id)._to;
+
+        // if node found is time and not location
+        if (db.time_period.exists(timePeriod_id)) {
+            var startStamp = db.time_stamp.document(db.start.outEdge(timePeriod_id)._to);
+            if (startStamp.value == start_time) {
+                var endStamp = db.time_stamp.document(db.end.outEdge(timePeriod_id)._to);
+                if (endStamp.value == end_time) {
+                    throw new error.NotAllowedError("Time period suggestion already exists for this activity. Duplicate suggestions");
+                }
+            }
+        }
+    });
+
     var suggestion_node = (new sug.Suggestion()).saveTimeSuggestion(start_time, end_time);
     var suggestedObject = {fromField:activity_id, toField:suggestion_node._id};
 
@@ -42,6 +59,20 @@ Suggested.prototype.saveSuggestedLocationEdge = function(activity_id, latitude, 
     var fromField = this.FROM_FIELD;
     var toField = this.TO_FIELD;
     var result;
+
+    // check to see if suggestion already exists
+    db.suggested.outEdges(activity_id).forEach(function(edge) {
+        var suggestion_id = edge._to;
+        var location_id = db.is.outEdges(suggestion_id)._to;
+
+        // if node found is location and not time
+        location = db.location.document(location_id);
+        if (location != null) {
+            if (location.latitude == latitude && location.longitude == longitude) {
+                throw new error.NotAllowedError("Location suggestion already exists for this activity. Duplicate suggestions");
+            }
+        }
+    });
 
     var suggestion_node = (new sug.Suggestion()).saveLocationSuggestion(latitude, longitude);
     var suggestedObject = {fromField:activity_id, toField:suggestion_node._id};
