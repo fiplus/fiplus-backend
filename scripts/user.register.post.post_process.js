310b2a386b0194d67a2c26ffa7e5dc9754e03229
Sean Mikalson <sean.mikalson@gmail.com>
2015-01-06 00:31:14 -0700

    Update script that fwds DF register user to arango to use correct url

--- scripts/user.register.post.post_process.js
+++ scripts/user.register.post.post_process.js
@@ -2,6 +2,6 @@ var data = {
     "email": event.response.email
 };
 
-var result = platform.api.post("http://localhost:8529/_db/fiplus/extensions/user/register", JSON.stringify(data));
+var result = platform.api.post("http://localhost:8529/_db/fiplus/extensions/userfi/register", JSON.stringify(data));
 
 print(result + "\n");
