c4e737015374de802d98c02bd62049c45048ba8b
Beenish Khurshid <kbeenish@hotmail.com>
2015-01-07 00:50:21 -0700

    Extract Models

--- fiplus-arango-extensions/apps/databases/fiplus/extensions/controllers/responseController.js
+++ fiplus-arango-extensions/apps/databases/fiplus/extensions/controllers/responseController.js
@@ -1,23 +1,17 @@
 var foxx = require("org/arangodb/foxx");
 var joi = require("joi");
+var model = require("model");
 
 (function() {
    'use strict';
 
     var controller = new foxx.Controller(applicationContext);
-
-    var InterestArrayModel = foxx.Model.extend({
-       schema: {
-           interests: joi.array()
-       }
-    });
-
     /*
      * responses
      */
     controller.put('/', function(request, response) {
         // Empty as this request is only here as a placeholder for response models
     }).bodyParam('InterestResponse', {
-        type: InterestArrayModel
+        type: model.InterestArrayModel
     });
 }());
