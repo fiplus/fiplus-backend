6387db8897cd4dad61576ee939df3757b21a3d3d
Beenish Khurshid <kbeenish@hotmail.com>
2015-01-19 22:43:13 -0700

    Remove DreamFactory
    forever and ever.

--- scripts/user.register.post.post_process.js
+++
@@ -1,7 +0,0 @@
-var data = {
-    "email": event.response.email
-};
-
-var result = platform.api.post("http://localhost:8529/_db/fiplus/extensions/userfi/register", JSON.stringify(data));
-
-print(result + "\n");
