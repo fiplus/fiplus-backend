226fd12166714529d00f945ea4ba12388e907b92
Sean Mikalson <sean.mikalson@gmail.com>
2015-02-27 12:48:02 -0700

    Refactor push notification

--- proxy-fiplus/server/boot/push-notification.js
+++ proxy-fiplus/server/boot/push-notification.js
@@ -2,12 +2,30 @@ var gcm = require('node-gcm-service');
 var query = require('./query');
 var push_message = require("./push-message");
 
+function sendPushNotification(message, deviceIds)
+{
+  var gcmSender = new gcm.Sender();
+  gcmSender.setAPIKey("AIzaSyDwbfeTyVbI1GvMh0JLNyweaNhSbqbgMzI");
+
+  deviceIds.forEach(function(id) {
+    gcmSender.sendMessage(message.toString(), id, true, function (err, data) {
+      // TODO More robust error/gcm result handling, i.e deleting invalid reg ids
+      if (!err) {
+        // do something
+        console.info("data", JSON.stringify(data));
+      } else {
+        // handle error
+        console.info("error " + err);
+      }
+    });
+  });
+}
+
 exports.SendNotificationOnActivityCreate = function(activity)
 {
   var createActivityResponse = JSON.parse(activity);
   query.getDeviceIdsInterestedInActivity(createActivityResponse.activity_id, function(err, devices) {
-    var gcmSender = new gcm.Sender();
-    gcmSender.setAPIKey("AIzaSyDwbfeTyVbI1GvMh0JLNyweaNhSbqbgMzI");
+
     var message = new push_message.NewActivityMessage(createActivityResponse.activity_id, createActivityResponse.Name + ' event created!');
     console.log(JSON.stringify(message));
     var gcmMessage = new gcm.Message({
@@ -15,18 +33,7 @@ exports.SendNotificationOnActivityCreate = function(activity)
       dry_run: false
     });
 
-    devices.forEach(function(id) {
-      gcmSender.sendMessage(gcmMessage.toString(), id, true, function (err, data) {
-        // TODO More robust error/gcm result handling, i.e deleting invalid reg ids
-        if (!err) {
-          // do something
-          console.info("data", JSON.stringify(data));
-        } else {
-          // handle error
-          console.info("error " + err);
-        }
-      });
-    });
+    sendPushNotification(gcmMessage, devices);
   });
 
 };
